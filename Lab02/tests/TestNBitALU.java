c20917d1bcbf01be2e413af8dc59fdd25939814f
Michael Palmer <michael@michaeldpalmer.com>
2017-02-04 12:41:31 -0500

    more tests!

--- Lab02/tests/TestNBitALU.java
+++ Lab02/tests/TestNBitALU.java
@@ -12,6 +12,34 @@ public class TestNBitALU extends TestCase {
         this.operations = this.alu.generateOperations();
     }
 
+    public void testGenerateOperations() {
+        this.alu = new NBitALU(4);
+        this.operations = this.alu.generateOperations();
+        boolean[][] operations = {
+                {false, false, false, false},
+                {false, false, false, true},
+                {false, false, true, false},
+                {false, false, true, true},
+                {false, true, false, false},
+                {false, true, false, true},
+                {false, true, true, false},
+                {false, true, true, true},
+                {true, false, false, false},
+                {true, false, false, true},
+                {true, false, true, false},
+                {true, false, true, true},
+                {true, true, false, false},
+                {true, true, false, true},
+                {true, true, true, false},
+                {true, true, true, true},
+        };
+        for (int i = 0; i < operations.length; i++) {
+            for (int j = 0; j < operations[i].length; j++) {
+                assertEquals(operations[i][j], this.operations[i][j]);
+            }
+        }
+    }
+
     public void testAddition() {
         for (boolean[] operationA : operations) {
             alu.setA(operationA);
